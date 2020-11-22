91b96f8e16f2c5f2ec675b53c9c6bf0714f689d2
noah <crashlive112@gmail.com>
2020-11-22 13:29:40 -0500

    commit tree 07c80485e55d3e0a0d5af5dae389eebb24de4075

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+    i = 0
+    new_collection = []
+    while i < array.length
+        yield(array[i])
+        new_collection << yield(array[i])
+        i +=1
+    end
+    new_collection
+end
 
 
